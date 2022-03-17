91d864cbf784bfd5eaa04e52ce2ce823ea8b77e0
AsyrafbilalFBJ <asyrafbilalrpltelu@student.telkomuniversity.ac.id>
2022-03-17 14:20:33 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul4_1302200010/Program.cs
+++ tpmodul4_1302200010/Program.cs
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace tpmodul4_1302200010
 {
+    class HaloGeneric
+    {
+        public static void SapaUser()
+        {
+            Console.Write("Masukan Nama : ");
+            string nama = Console.ReadLine();
+            Console.WriteLine("Halo user "+nama);
+            Console.ReadKey();
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            HaloGeneric.SapaUser();
         }
     }
 }
